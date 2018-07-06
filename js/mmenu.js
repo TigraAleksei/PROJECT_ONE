cfaf13a410cc7da109db1b9eca785bec062f9443
TigraAleksei <kuleshovaleksei@bk.ru>
2018-07-06 16:15:49 +0300

    06.07.2018 16: 15

--- js/mmenu.js
+++ js/mmenu.js
@@ -1,7 +1,10 @@
 $(function() {
 //	create a menu
 $('#menu').mmenu({
-	extensions: ['widescreen', 'theme-black', 'effect-menu-slide', 'pagedim-black', 'position-right']
+	extensions: ['widescreen', 'theme-black', 'effect-menu-slide', 'pagedim-black', 'position-right'],
+	navbar: {
+		title: '<img src="../img/airplane-shape.svg" alt="Самолет">'
+	}
 });
 //	fire the plugin
 $('.mh-head.first').mhead({
