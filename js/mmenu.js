d1147843f3263abe12bc617f2c3a964bb3907552
TigraAleksei <kuleshovaleksei@bk.ru>
2018-07-18 12:15:58 +0300

    18.07.2018 12:15

--- js/mmenu.js
+++ js/mmenu.js
@@ -2,9 +2,6 @@ $(function() {
 //	create a menu
 $('#menu').mmenu({
 	extensions: ['widescreen', 'theme-black', 'effect-menu-slide', 'pagedim-black', 'position-right'],
-	navbar: {
-		title: '<img src="../img/airplane-shape.svg" alt="Самолет">'
-	}
 });
 //	fire the plugin
 $('.mh-head.first').mhead({
